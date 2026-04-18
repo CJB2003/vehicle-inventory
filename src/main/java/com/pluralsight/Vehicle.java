21347b0639bce601fc751cbb30052f403f858185
Chris Bunny <christopherjbunny@gmail.com>
2026-04-18 13:09:23 -0500

    Preloaded the array with vehicles. Created method that displays available vehicles in inventory, created a for loop that counts how many vehicles are in the inventory

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,61 @@
 package com.pluralsight;
 
 public class Vehicle {
+
+    //Created 5 private properties
+    private long vehicleID;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private double price;
+
+    //generated constructor for object
+    public Vehicle(long vehicleID, String makeModel, String color, int odometerReading, double price) {
+        this.vehicleID = vehicleID;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
+    }
+
+    //getters and setters
+    public long getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(long vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/pluralsight/VehicleApp.java
+++ src/main/java/com/pluralsight/VehicleApp.java
@@ -1,4 +1,81 @@
 package com.pluralsight;
+import java.util.*;
+import java.util.Arrays;
 
 public class VehicleApp {
+
+    static Scanner myScanner = new Scanner(System.in);
+
+    //static vehicle array that can be accessed throughout all methods
+    static Vehicle[] vehicles = new Vehicle[20];
+
+    public static void main(String[] args) {
+
+        //initialized variable for vehicle counter
+        int vehicleCounter = 0;
+
+        //preloading vehicle array with 6 vehicles
+        vehicles[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000,13500);
+        vehicles[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+        vehicles[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+        vehicles[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+        vehicles[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+        vehicles[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
+
+        //for loop that iterates through the array, checks if the vehicle index isn't null, if not then the counter goes up
+        for (int i = 0; i < vehicles.length; i++) {
+
+            if (vehicles[i] != null) {
+                vehicleCounter++;
+            }
+        }
+        //Prints out the amount of vehicles we have in inventory
+        System.out.println("We have " + vehicleCounter + " vehicles in our inventory.\n");
+
+        //
+        while (true) {
+            System.out.println("What do you want to do?");
+            System.out.println(" 1 - List all vehicles");
+            System.out.println(" 2 - Search by make/model");
+            System.out.println(" 3 - Search by price range");
+            System.out.println(" 4 - Search by color");
+            System.out.println(" 5 - Add a vehicle");
+            System.out.println(" 6 - Quit");
+            System.out.println("Enter your command: ");
+            int command = myScanner.nextInt();
+            myScanner.nextLine();
+
+            switch(command) {
+                case 1:
+                    listAllVehicles(vehicles, vehicleCounter);
+                    break;
+                case 2:
+                    findVehiclesByPrice();
+                    break;
+                case 5:
+                    addAVehicle();
+                    break;
+                case 6:
+                    System.exit(0);
+            }
+        }
+
+    }
+    //method that displays AVAILABLE vehicles in inventory
+    public static void listAllVehicles(Vehicle[] vehicles, int vehicleCounter) {
+
+        System.out.println("Here are the vehicles listed below.");
+        for (int i = 0; i < vehicleCounter; i++) {
+
+            System.out.println(vehicles[i] + "\n");
+        }
+    }
+
+    public static void findVehiclesByPrice() {
+
+    }
+
+    public static void addAVehicle() {
+
+    }
 }
